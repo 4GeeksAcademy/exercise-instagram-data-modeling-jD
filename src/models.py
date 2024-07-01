dbbff95720665acee0f66af6c5a3552b557887f9
jdOJ <162026593+jdOJ@users.noreply.github.com>
2024-07-01 19:24:54 +0000

    creo que si

--- src/models.py
+++ src/models.py
@@ -7,93 +7,69 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class User(Base):
-    __tablename__ = "user"
-    id = Column(Integer, primary_key=True)
-    user_name = Column(String(250), index=True)
-    first_name = Column(String(250))
-    last_name = Column(String(250))
-    email = Column(String(250), nullable=False)
-    
-    followers = relationship(
-        'Follower',
-        foreign_keys='Follower.user_to_id',
-        back_populates='user_to'
-    )
-    
-    followings = relationship(
-        'Follower',
-        foreign_keys='Follower.user_from_id',
-        back_populates='user_from'
-    )
+
 
 class Follower(Base):
     __tablename__ = "follower"
-    id = Column(Integer, primary_key=True)
-    user_from_id = Column(Integer, ForeignKey("user.id"))
-    user_to_id = Column(Integer, ForeignKey("user.id"))
+    user_from_id= Column(Integer, ForeignKey('user.id'), nullable = False, primary_key = True)
+    user_to_id = Column(Integer,ForeignKey('user.id'), nullable = False, primary_key = True)
+    
+    
 
-    user_from = relationship('User', foreign_keys=[user_from_id], back_populates='followings')
-    user_to = relationship('User', foreign_keys=[user_to_id], back_populates='followers')
+
+class User(Base):
+    __tablename__ = 'user'
+    # Here we define columns for the table person
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    username = Column(String(250), nullable=False)
+    firstname = Column(String(250), nullable=False)
+    lastname = Column(String(250), nullable=False)
+    email = Column(String(250),unique=True, nullable = False)
+
+    followed = relationship(
+        'User',
+        secondary= 'Follower',
+        primaryjoin = (Follower.user_from_id == id),
+        secondaryjoin = (Follower.user_to_id == id),
+        backref="following",
+        lazy='dynamic'
+    )
+
+
+
+
+
 class Post(Base):
-    __tablename__="post"
+    __tablename__ = "post"
     id = Column(Integer, primary_key=True)
-    user_id= Column(Integer,ForeignKey("User.id"))
-    user= relationship(User)
-
-class Comment(Base):
-    __tablename__="comment"
-    id = Column(Integer, primary_key=True)
-    comment_text = Column(String(250))
-    author_id = Column(Integer, ForeignKey("User.id"))
-    post_id= Column (Integer, ForeignKey("Post.id"))
-    user= relationship(User)
-    post= relationship(Post)
-
-
+    user_id = Column(Integer, ForeignKey('user.id'), nullable = False)
+    post = relationship(User)
 
 class Media(Base):
-    __tablename__= "media"
-    id= Column(Integer, primary_key=True)
-    type= Column(Enum("like","comment"),nullable=False)
-    url= Column(String(250))
-    post_id= Column(Integer, ForeignKey("Post.id"))
+    __tablename__ = "media"
+    id = Column(Integer, primary_key = True)
+    type= Column(Enum)
+    url = Column(String)
+    post_id = Column(Integer, ForeignKey('post.id'), nullable = False)
+    media = relationship(Post)
 
+class Comment(Base):
+    __tablename__ = 'comment'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    comment_text = Column(String(250))
+    author_id = Column(Integer , ForeignKey('user.id'), nullable = False)
+    post_id = Column(Integer, ForeignKey('post.id'), nullable = False )
+    #person_id = Column(Integer, ForeignKey('person.id'))
+    #person = relationship(Person)
+    comment_user = relationship(User)
+    comment_post = relationship(Post)
 
-
-#         return {}
-
-# class Media (Base):
-#     __tablename__=""
-#     id= Column(Integer, ForeignKey("Post.id"))
-#     like_
-
-
-
-# class User(Base):
-#     __tablename__ = 'user'
-#     # Here we define columns for the table person
-#     # Notice that each column is also a normal Python instance attribute.
-#     id = Column(Integer, primary_key=True)
-#     user_name= Column(String(250), index=True),
-#     first_name= Column(String(250), nullable=False),
-#     last_name= Column(String(250), nullable=False),
-#     email= Column(String(250), nullable=False),
-
-
-# class Address(Base):
-#     __tablename__ = 'address'
-#     # Here we define columns for the table address.
-#     # Notice that each column is also a normal Python instance attribute.
-#     id = Column(Integer, primary_key=True)
-#     street_name = Column(String(250))
-#     street_number = Column(String(250))
-#     post_code = Column(String(250), nullable=False)
-#     person_id = Column(Integer, ForeignKey('person.id'))
-#     person = relationship(Person)
-
-#     def to_dict(self):
-#         return {}
+    def to_dict(self):
+        return {}
+    
 
 ## Draw from SQLAlchemy base
 try:
