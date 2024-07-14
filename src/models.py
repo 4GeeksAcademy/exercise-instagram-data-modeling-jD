227f2bdad1c29d9d0f1aa991f6b8848e374ef5f9
jdOJ <162026593+jdOJ@users.noreply.github.com>
2024-07-14 20:46:28 +0200

    Update models.py

--- src/models.py
+++ src/models.py
@@ -13,8 +13,7 @@ class Follower(Base):
     __tablename__ = "follower"
     user_from_id= Column(Integer, ForeignKey('user.id'), nullable = False, primary_key = True)
     user_to_id = Column(Integer,ForeignKey('user.id'), nullable = False, primary_key = True)
-    user_from = relationship("User", foreign_keys=[user_from_id], back_populates="following")
-    user_to = relationship("User", foreign_keys=[user_to_id], back_populates="followers")
+
     
 
 
@@ -27,10 +26,15 @@ class User(Base):
     firstname = Column(String(250))
     lastname = Column(String(250))
     email = Column(String(250),unique=True, nullable = False)
-    posts = relationship("Post", back_populates="user")
-    comments = relationship("Comment", back_populates="author")
-    followers = relationship("Follower", foreign_keys="Follower.user_to_id", back_populates="user_to")
-    following = relationship("Follower", foreign_keys="Follower.user_from_id", back_populates="user_from")
+    
+    followers = relationship(
+        'User',
+        secondary= 'Follower',
+        primaryjoin = (Follower.user_from_id == id),
+        secondaryjoin = (Follower.user_to_id == id),
+        backref="following",
+        lazy='dynamic'
+    )
 
 
 
@@ -50,17 +54,15 @@ class Media(Base):
     post = relationship("Post", back_populates="media")
 
 class Comment(Base):
-    __tablename__ = 'comment'
+      __tablename__ = 'comment'
     # Here we define columns for the table address.
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    comment_text = Column(String(250), nullable=False)
+    comment_text = Column(String(250))
     author_id = Column(Integer , ForeignKey('user.id'), nullable = False)
     post_id = Column(Integer, ForeignKey('post.id'), nullable = False )
-    #person_id = Column(Integer, ForeignKey('person.id'))
-    #person = relationship(Person)
-    author = relationship("User", back_populates="comment")
-    post = relationship("Post", back_populates="comment")
+    comment_user = relationship(User)
+    comment_post = relationship(Post)
     def to_dict(self):
         return {}
     
